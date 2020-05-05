56203748e6de23881c8080ee18b59b6e28d4e667
Mofizur Rahman <moficodes@gmail.com>
2020-05-05 00:34:12 -0400

    update select os version radio

--- client/src/components/common/CreateForm.js
+++ client/src/components/common/CreateForm.js
@@ -6,21 +6,85 @@ import {
   Button,
   Dropdown,
   TileGroup,
-  RadioTile
+  RadioTile,
+  Row,
+  Grid,
+  Column,
+  FormLabel,
+  Tooltip,
 } from "carbon-components-react";
+
+import styles from "./CreateForm.module.css";
 
 import "./CreateForm.css";
 
 const CreateForm = () => {
+  const [kubernetesSelected, setKubernetesSelected] = React.useState(true);
+  const [openshiftSelected, setOpenshiftSelected] = React.useState(false);
+
+  const toggleRadio = () => {
+    setKubernetesSelected(!kubernetesSelected);
+    setOpenshiftSelected(!openshiftSelected);
+  };
+
   return (
     <Form>
-      <TextInput id="1" labelText="1" />
-      <TextInput id="2" labelText="2" />
-      <TextInput id="3" labelText="3" />
-      <TextInput id="4" labelText="4" />
-      <TextInput id="5" labelText="5" />
-      <TextInput id="6" labelText="6" />
-      <TextInput id="7" labelText="7" />
+      <Grid>
+        <FormLabel>
+          <Tooltip triggerText="Cluster type and version">
+            The container platform type and version for the cluster. Choose
+            Kubernetes for a native Kubernetes experience on Ubuntu, or
+            OpenShift to deliver Kubernetes apps on Red Hat Enterprise Linux.
+          </Tooltip>
+        </FormLabel>
+        <Row>
+          <Column md={4} lg={3}>
+            <RadioTile
+              onClick={toggleRadio}
+              checked={kubernetesSelected}
+              className={styles.radio_tile}
+            >
+              <div className="radio-tile-content">
+                <img
+                  className="radio-tile-image"
+                  src="https://i.ibb.co/cDqxKBd/download.png"
+                  height={100}
+                  width={100}
+                />
+                <p>Kubernetes</p>
+              </div>
+              <Dropdown
+                disabled={!kubernetesSelected}
+                label="Select Version"
+                items={["1", "2", "3"]}
+              />
+            </RadioTile>
+          </Column>
+          <Column md={4} lg={3}>
+            <RadioTile
+              onClick={toggleRadio}
+              checked={openshiftSelected}
+              className={styles.radio_tile}
+            >
+              <div className="radio-tile-content">
+                <img
+                  className="radio-tile-image"
+                  src="https://i.ibb.co/0fFQCD2/openshift.png"
+                  height={100}
+                  width={100}
+                />
+                <p>OpenShift</p>
+              </div>
+              <Dropdown
+                disabled={!openshiftSelected}
+                label="Select Version"
+                className={styles.dropdown}
+                items={["1", "2", "3"]}
+              />
+            </RadioTile>
+          </Column>
+        </Row>
+      </Grid>
     </Form>
   );
 };
--- client/src/components/common/Navbar.js
+++ client/src/components/common/Navbar.js
@@ -1,21 +1,26 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 import { Dropdown } from "carbon-components-react";
+// import Dropdown from "react-dropdown";
 import history from "../../globalHistory";
 import "./Dropdown.css";
 
-const MenuItem = props => {
+const MenuItem = (props) => {
   return (
     <>
-      <div className={styles.menuItem} style={props.style} onClick={props.onClickHandler}>
+      <div
+        className={styles.menuItem}
+        style={props.style}
+        onClick={props.onClickHandler}
+      >
         {props.label}
       </div>
     </>
   );
-}
+};
 
-const Navbar = props => {
-  const itemToString = item => {
+const Navbar = (props) => {
+  const itemToString = (item) => {
     if (item) {
       let name = item.entity.name;
       let softlayerAccountId =
@@ -35,17 +40,23 @@ const Navbar = props => {
 
   const homeClick = () => {
     history.push("/");
+  };
+
+  const options = (opts) => {
+    console.log(opts);
   }
 
+  console.log(props.items);
+  // const defaultOption = options[0];
 
   return (
     <>
       <div className={styles.wrapper}>
-        <div className={styles.title} onClick={homeClick} >
+        <div className={styles.title} onClick={homeClick}>
           <span className={styles.bold}>IBM</span> Cloud
         </div>
-        <MenuItem label="Create" onClickHandler={handleCreateClick}/>
-        <MenuItem label="Schedule" onClickHandler={handleScheduleClick}/>
+        <MenuItem label="Create" onClickHandler={handleCreateClick} />
+        <MenuItem label="Schedule" onClickHandler={handleScheduleClick} />
         <Dropdown
           disabled={props.accountsLoaded}
           className={styles.dropdown}
@@ -54,7 +65,7 @@ const Navbar = props => {
           items={props.items || []}
           onChange={props.accountSelected}
           itemToString={itemToString}
-          id="account-dropdown"
+          // id="account-dropdown"
           light={false}
         />
       </div>
