7fc4312a219a099218d8fc7fd4072e36cc58fe38
Mofizur Rahman <moficodes@gmail.com>
2020-05-13 22:34:56 -0400

    added travis tag

--- .travis/build-and-push.sh
+++ .travis/build-and-push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-docker build -t moficodes/ibmcloud-kubernetes-admin:$TRAVIS_COMMIT .
+echo "Building for Commit : $TRAVIS_COMMIT  and Tag : $TRAVIS_TAG"
+docker build -t moficodes/ibmcloud-kubernetes-admin:$TRAVIS_TAG .
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker push moficodes/ibmcloud-kubernetes-admin:$TRAVIS_COMMIT
+docker push moficodes/ibmcloud-kubernetes-admin:$TRAVIS_TAG
