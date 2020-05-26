882ff3122d29bbf5b5dd55d8b48f70a1bc4f5413
Mofizur Rahman <moficodes@gmail.com>
2020-05-26 17:24:58 -0400

    tidy up imports

--- internals/server/cluster.go
+++ internals/server/cluster.go
@@ -3,7 +3,6 @@ package server
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -61,8 +60,6 @@ func (s *Server) ClusterCreateHandler(w http.ResponseWriter, r *http.Request) {
 		handleError(w, http.StatusUnauthorized, "could not create cluster", err.Error())
 		return
 	}
-
-	log.Println("cluster created :", createResponse.ID)
 
 	w.WriteHeader(http.StatusOK)
 	e := json.NewEncoder(w)
